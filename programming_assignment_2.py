d04621ba0d6c9dfd18d33359ecf0bcedb521c966
saramsv <sara.mousavi.ch@gmail.com>
2016-10-28 16:16:07 -0400

    added CBC decryption

--- programming_assignment_2.py
+++ programming_assignment_2.py
@@ -5,27 +5,86 @@ import binascii
 import struct
 import math
 key = '0123456789abcdef'
-
-#IV = 16 * '\x00'
-IV = Random.get_random_bytes(16) # CHECK!!
-
-mode = AES.MODE_ECB
-encryptor = AES.new(key)
 block_length = 16
-message= 'sarakai' # pading and creating blocks
-message = message + chr(0) * (block_length - len(message))
-num_block = math.ceil(len(message)/float(block_length))
-message_blocks = []
-cipher_blocks = []
-message_blocks.append(message)
-IV  = IV + chr(0) * (block_length - len(IV))
+xorWord = lambda ss,cc: ''.join(chr(ord(s)^ord(c)) for s,c in zip(ss,cc))
 
-for m in message_blocks:
+def generate_iv(block_length):
+    #IV = 16 * '\x00'
+    IV = Random.get_random_bytes(block_length) # CHECK!!
+    IV = binascii.hexlify(IV).decode()
+    #print (type(IV))
+    return IV
+
+def pad(plaintext,block_length):
+    num_padzeros = len(plaintext) % block_length
+    plaintext = plaintext + chr(0)*num_padzeros
+    num_blocks = len(plaintext)/block_length
+    message_blocks = []
+    for i in range(num_blocks):
+        message_blocks.append(binascii.hexlify(plaintext[i*block_length:(i+1)*block_length]).decode())
+    return message_blocks
+
+def CBC_encryption(plaintext , key):
+    mode = AES.MODE_ECB
+    encryptor = AES.new(key)
     xorWord = lambda ss,cc: ''.join(chr(ord(s)^ord(c)) for s,c in zip(ss,cc))
-    encrypt = xorWord(message,IV)
-    ciphertext = encryptor.encrypt(encrypt)
-    IV = ciphertext
-    cipher_blocks.append(ciphertext)
-    ciphertext = binascii.hexlify(ciphertext).decode()
-    print (ciphertext)
+    IV = generate_iv(block_length)  
+    IV_init = IV
+    cipher_blocks = []
+    for plaintext_i in plaintext:
+        XOR_message = xorWord(plaintext_i,IV)
+        cipher_blocks.append(encryptor.encrypt(XOR_message))
+        IV = cipher_blocks[-1]
+    return cipher_blocks,IV_init
+
+def CBC_decryption(ciphertext,key, IV_init):
+    mode = AES.MODE_ECB
+    decryptor = AES.new(key)
+    decrypted_plaintext = [0]*len(ciphertext)
+    for i in range(len(ciphertext)): 
+        if i != 0:
+            IV = ciphertext[i-1]
+        else:
+            IV = IV_init
+        decrypted_plaintext[i] = binascii.unhexlify(xorWord(decryptor.decrypt(ciphertext[i]), IV))      
+    return decrypted_plaintext
+
+'''def CTR_encryption(plaintext, key):
+    mode = AES.MODE_ECB
+    encryptor = AES.new(key)
+    xorWord = lambda ss,cc: ''. join(chr(ord(s)^ord(c)) for s,c in zip(ss,cc))
+    IV = generate_iv(block_length)
+    IV_init = IV
+    cipher_blocks = []
+    for plaintext_i in plaintext:
+        cipher_blocks.append(xorWord(encryptor.encrypt(IV),plaintext_i))
+        print '1'
+        print IV
+        IV = int(binascii.hexlify(IV).decode()) + 1
+        print 'iv+1'
+        print IV
+        #IV = str(IV)
+        #IV = binascii.hexlify(IV).decode('hex')
+        print str(IV).decode("utf-8")
+        #print IV
+    return cipher_blocks'''    
+
+                
+
+if __name__=='__main__':
+
+    plaintext = 'sarakai' * 15
+    message_blocks = pad(plaintext,block_length)
+    ciphertext,iv = CBC_encryption(message_blocks, key)
+    decrypted_plaintext = CBC_decryption(ciphertext, key, iv)
+    print (decrypted_plaintext)
     
+    #CTR_encryption(plaintext, key)
+    '''IV = generate_iv(block_length)
+    IV = int(IV) + 1
+    print (type(IV))
+    print IV'''
+    
+
+
+
