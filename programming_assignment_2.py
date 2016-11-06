17aba82dd981983d6e9d1f58601493161f4feb60
saramsv <sara.mousavi.ch@gmail.com>
2016-11-06 01:14:48 -0500

    added one more step to the prime generator and some changes to CTR_cipher

--- programming_assignment_2.py
+++ programming_assignment_2.py
@@ -5,7 +5,8 @@ import binascii
 import struct
 import math
 from Crypto.Hash import MD5
-import random
+from Crypto.Random import random
+
 k = open('exampleKeyOnes.txt')
 key = k.read()
 key = binascii.unhexlify(key)
@@ -26,28 +27,37 @@ def prime_test(p):
         if N % 2 == 1:
             d = N
             break
-    a = random.randint(1, p-1)
-    flag = False
-    for r in range(s):
-        if r == 0:
-            w = pow(a, d, p)
-            if w == 1:
+    for i in range(5):
+        a = random.randint(1, p-1)
+        flag = False
+        for r in range(s):
+            if r == 0:
+                w = pow(a, d, p)
+                if w == 1:
+                    flag = True
+                    break
+            q = pow(a, (2**r)*d , p)
+            if q == p-1:
                 flag = True
                 break
-        q = pow(a, (2**r)*d , p)
-        if q == p-1:
-            flag = True
-            break
     return flag
 
 def generate_prime(num_bits):
     k = num_bits  
+    i = 0
+    prime_less_than_1000 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677,683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
     while True:
         p = random.randrange(2**(k-1),2**(k))
-
-        prime_number_p = prime_test(p)
-        if prime_number_p == True :
-            return p
+        check = 0
+        for i in prime_less_than_1000:
+            if p%i == 0:
+                check = 1
+        #i = i+1
+        if check == 0:
+            prime_number_p = prime_test(p)
+            if prime_number_p == True :
+                #print i
+                return p
 
 def pad(plaintext,block_length):
     num_padzeros = len(plaintext) % block_length
@@ -99,17 +109,25 @@ def CTR_encryption(plaintext, key):
         IV = binascii.hexlify(IV)
         IV = int(IV, 16) + 1
         IV = Dump(IV)
-    return cipher_blocks
+    return cipher_blocks,IV_init
 
 def CTR_decryption(ciphertext, key, IV):
     mode = AES.MODE_ECB
-    decryptor = AES.new(key)
-    decrypted_plaintext = [0]*len(ciphertext)
+    encryptor = AES.new(key)
+    decrypted_plaintext =[]# [0]*len(ciphertext)
+    '''
     for i in range(len(ciphertext)): 
         IV = binascii.hexlify(IV)
         IV = int(IV, 16) + 1
         IV = Dump(IV)
-        decrypted_plaintext[i] = ecryptor.decrypt(xorWord(dciphertext[i], IV))
+        decrypted_plaintext[i] = xorWord(ciphertext[i],encryptor.encrypt(IV))
+    '''
+    for c in ciphertext:
+        IV = binascii.hexlify(IV)
+        IV = int(IV, 16) +1
+        IV = Dump(IV)
+        decrypted_plaintext.append(xorWord(c,encryptor.encrypt(IV)))
+
     return decrypted_plaintext
 
 def to_bytes(n, length, endianess='big'): # the input number's length in bits  has to be less than block length 
@@ -179,26 +197,66 @@ def Hash_and_mac_verificaion(message, key, tag):
         print 'invalid tag'
         return
 
+def check_relatively_prime(a,b):
+    while b: #b != 0
+        a,b = b, a%b
+    return a == 1
+
+def make_key_pair(p,q): 
+    #N = p * q
+    phi_N = (p - 1) * (q - 1)
+    '''
+    e = random.randrange(0, phi_N)
+    while True:
+        if check_relatively_prime(e, phi_N):
+            break
+         else:
+            e = random.randrange(0, phi_N)
+            print "this is e"
+    print e'''
+    e = 65537   
+    if check_relatively_prime(e, phi_N)==False:
+        print "This e doesnot work"
+    d = 3
+    print e, phi_N
+    while d < phi_N:
+        a = (d * e) % phi_N
+        #print a
+        if  a == 1:
+            break 
+        d = d + 1
+    return p*q, phi_N, d, e
+
+
 if __name__=='__main__':
 
+    
     # Encrypt using CBC mode
     input_file = open('exampleInputA.txt')
     plaintext = input_file.read()
     message_blocks = pad(plaintext,block_length)
     ciphertext,iv = CBC_encryption(message_blocks, key)
-    print "CBC_encryption{}".format(ciphertext)
+    #output_file = open('CBC_encryption.txt', 'w' )
+    #ciphertext_string = ''.join(ciphertext)
+    #output_file.write(ciphertext_string)
+    #print "CBC_encryption{}".format(ciphertext)
     #print "Cipher text: {}".format(ciphertext)
     # decrypt using CBC mode
     decrypted_plaintext = CBC_decryption(ciphertext, key, iv)
-    print ''.join(decrypted_plaintext)
+    dec =  ''.join(decrypted_plaintext)
+    output_file = open('CBC_decryption.txt', 'w' )
+    output_file.write(dec)
+
 
     # Encrypt using CTR mode
-    ciphertext,iv = CBC_encryption(message_blocks, key)
-    print "{}".format(plaintext)
+    print plaintext
+    ciphertext,iv = CTR_encryption(message_blocks, key)
+    #print "{}".format(ciphertext)
     #print CTR_encryption(message_blocks, key)
     # decrypt using CTR mode
-    decrypted_plaintext = CBC_decryption(ciphertext, key, iv)
-    print ''.join(decrypted_plaintext)
+    decrypted_plaintext = CTR_decryption(ciphertext, key, iv)
+    p =  ''.join(decrypted_plaintext)
+    print binascii.hexlify(p)
 
     # generating a mac and verify it using CBC_mac
     tag = CBC_mac(plaintext , key)
@@ -209,7 +267,15 @@ if __name__=='__main__':
     print "mac resulted from hash and mac{}".format(binascii.hexlify(Hash_and_mac(plaintext , key)))
     tag2 = 'hfueie'
     Hash_and_mac_verificaion(plaintext , key , tag2)
-    print generate_prime(1024)
+    
+    p = generate_prime(1024)
+    q = generate_prime(1024)
+    print p
+    print q
+    #n, phi, d, e = make_key_pair(p, q)
+    #print n, phi, d, e
+    
+
 
 
 
